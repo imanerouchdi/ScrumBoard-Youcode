6cf797196c079022e47d385a11a0ee8ab17e1c8c
imanerouchdi <imane.rcdi@gmail.com>
2022-10-20 07:40:05 +0200

    create

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,74 @@
  * 
  */
 // afficher html+function affichage
+
+    const toDoTask=document.getElementById("to-do-tasks");
+    const inProgressTask=document.getElementById("in-progress-tasks");
+    const doneTask=document.getElementById("done-tasks");
+
 function createTask() {
+    toDoTask.innerHTML="";
+    inProgressTask="";
+    doneTask="";
+    tasks.forEach(task => {
+        if(task==="To Do"){
+            toDoTask.innerHTML+=`<button class="d-flex button border w-100 p-1 p-1"> 
+            <div class="col-md-1">
+                <i class="bi bi-question-circle text-success"></i>
+            </div>
+            <div class="text-start col-md-11 ">
+                <div class="fw-bold">${task.title}</div>
+                <div class="text ">
+                    <div class="text text-muted">${task.date}</div>
+                    <div class="text-Des" title="There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly.">${task.description}There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task.
+                    </div>
+                </div>
+                <div class="my-10px">
+                    <span class="btn btn-primary">${task.priority}</span>
+                    <span class="btn btn-gray-500 text-black">${task.type}</span>
+                </div>
+            </div>
+        </button>
+        `
+        }
+        else if (task.status === "In Progress") {
+            inProgress.innerHTML += `<button class="bg-white w-100 border-0 border-top d-flex py-2 ">
+            <div class="px-2 ">
+                <i class="bi bi-question-circle text-success fs-3 "></i> 
+            </div>
+            <div class="text-start ">
+                <div class="h6 ">${task.title}</div>
+                <div class="text-start ">
+                    <div class="text-gray ">#1 created in 2022-10-08</div>
+                    <div title="${task.description}"> ${task.description}</div>
+                </div>
+                <div class=" ">
+                    <span class="btn btn-primary ">High</span>
+                    <span class="btn btn-light text-black ">Feature</span>
+                </div>
+            </div>
+        </button>`
+        } else if (task.status === "Done") {
+            doneTask.innerHTML += `<button class="bg-white w-100 border-0 border-top d-flex py-2 ">
+            <div class="px-2 ">
+                <i class="bi bi-question-circle text-success fs-3 "></i> 
+            </div>
+            <div class="text-start ">
+                <div class="h6 ">${task.title}</div>
+                <div class="text-start ">
+                    <div class="text-gray ">#1 created in 2022-10-08</div>
+                    <div title="${task.description}"> ${task.description}</div>
+                </div>
+                <div class=" ">
+                    <span class="btn btn-primary ">High</span>
+                    <span class="btn btn-light text-black ">Feature</span>
+                </div>
+            </div>
+        </button>`
+        }
+    });
+
+
     // initialiser task form
 
     // Afficher le boutton save
@@ -12,56 +79,51 @@ function createTask() {
 
     // console.log("open the form ");
     
-    var titre=document.getElementById("titre").value;
-    var type=document.getElementsByName("type");
-    var priority=document.getElementById("Priority").value
-    var status=document.getElementById("Status").value;
-    var datte =document.getElementById("Date").value;
-    var description=document.getElementById("desc").value;
+    // var titre=document.getElementById("titre").value;
+    // var type=document.getElementsByName("type");
+    // var priority=document.getElementById("Priority").value
+    // var status=document.getElementById("Status").value;
+    // var datte =document.getElementById("Date").value;
+    // var description=document.getElementById("desc").value;
     // create variable for input 
     // checht type
-    var checkType; var afficherTask;
-    for(var item in type ){
-        if(type[item].checked==1)
-        checkType=type[item].value;
-    }
+    // var checkType; var afficherTask;
+    // for(var item in type ){
+    //     if(type[item].checked==1)
+    //     checkType=type[item].value;
+    // }
 
      // test of empty
-    if(priority=="" || status!="" || titre!=""|| !datte  || description!=""){
-        alert("veuillez remplir les champ");
-    }else{
-        // ajouter task in one of 3
-            // place of tasks 
-        if(status=='to-do') afficherTask=document.getElementById("to-do-tasks");
-        else if(status=='in-progress') afficherTask=document.getElementById("in-progress-tasks");
-        else if (status=='done') afficherTask=document.getElementById("done-tasks");}
-        let btn_addTask=document.getElementById("save");
-        // change the content of button
-        btn_addTask.innerHTML=`
-                        <div class="col-md-1">
-							<i class="bi bi-question-circle text-success"></i> 
-						</div>
-						<div class="text-start col-md-11">
-							<div class="fw-bold">${titre}</div>
-								<div class="text">
-									<div class="text-muted">#5 ${date}</div>
-                                    <div class="text-desc" title="There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly.">${desc}</div>
-								</div>
-								<div class="my-10px">
-										<span class="btn btn-primary">${priority}</span>
-										<span class="btn btn-gray-500 text-black" >${$type}</span>
-								</div>
-						</div>
-                        `;
-                        btn_addTask.classList.add("d-flex", "button", "border", "w-100", "p-1" ,"p-1");
-                        afficherTask.appendChild(btn_addTask);
-        
-        
-        
-        
-
-    }   
-
+    // if(priority=="" || status!="" || titre!=""|| !datte  || description!=""){
+    //     alert("veuillez remplir les champ");
+    // }else{
+    //     // ajouter task in one of 3
+    //         // place of tasks 
+    //     if(status=='to-do') afficherTask=document.getElementById("to-do-tasks");
+    //     else if(status=='in-progress') afficherTask=document.getElementById("in-progress-tasks");
+    //     else if (status=='done') afficherTask=document.getElementById("done-tasks");}
+    //     let btn_addTask=document.getElementById("save");
+    //     // change the content of button
+    //     btn_addTask.innerHTML=`
+    //                     <div class="col-md-1">
+	// 						<i class="bi bi-question-circle text-success"></i> 
+	// 					</div>
+	// 					<div class="text-start col-md-11">
+	// 						<div class="fw-bold">${titre}</div>
+	// 							<div class="text">
+	// 								<div class="text-muted">#5 ${date}</div>
+    //                                 <div class="text-desc" title="There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly.">${desc}</div>
+	// 							</div>
+	// 							<div class="my-10px">
+	// 									<span class="btn btn-primary">${priority}</span>
+	// 									<span class="btn btn-gray-500 text-black" >${$type}</span>
+	// 							</div>
+	// 					</div>
+    //                     `;
+    //                     btn_addTask.classList.add("d-flex", "button", "border", "w-100", "p-1" ,"p-1");
+    //                     afficherTask.appendChild(btn_addTask);
+    // }   
+}
 function saveTask() {
     // Recuperer task attributes a partir les champs input
 
@@ -85,17 +147,6 @@ function saveTask() {
     }
     tasks[tasks.lenght]=task;
     console.log(tasks);
-
-
-
-
-
-
-
-
-
-
-
 }
 
     
